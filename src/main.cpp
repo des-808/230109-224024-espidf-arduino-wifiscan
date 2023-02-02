6874fa3d01c5ef8a66471784c262937d3d72d06e
des-808 <des-808@inbox.com>
2023-02-03 01:54:55 +0300

    xz

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,32 @@
 #include "freertos/queue.h"
 #include "driver/uart.h"
 #include "sdkconfig.h"
-#include <WiFi.h>
+//#include <WiFi.h>
 #include "arduino.h"
 #include "ntp.h"
 #include "esp_timer.h"
 ////////////////////
 //#include "esp_netif_sntp.h"
-#include "lwip/ip_addr.h" 
-#include "esp_sntp.h"
+//#include "lwip/ip_addr.h" 
+//#include "esp_sntp.h"
 ////////////////////
 #include <stdint.h>
 #include "WiFiScan.h"
 HardwareSerial Serial_hmi(2); 
-#include "hmi_mini.h"  
+#include "hmi_mini.h" 
 
+
+#define STACK_SIZE 2048
+#define DELAY_1second 1000
+#define DELAY_1minutes 60000
+#define DELAY_1hour 360000
+#define DELAY_24hours 8640000
 NTP ntp(3);
 String incStr;
 String intToString(int tmp,int sistema_shislenyya);
-//void wifiScan();
-//String intToString(int tmp,int sistema_shislenyya);
-void ReadSerial_2();
-//void AnaliseString();
+void wifiScan();
+void ntp_status();
+void upload_clock_hmi();
 
 bool ssid_ok=false;
 bool password_ok=false;
@@ -37,8 +42,11 @@ int count = 0;
 bool read_buf_serial_hmi_bool =false;
 bool boolean_xz = false;
 bool xx = false;
-
-u_int8_t pinR = 8;
+//u_int8_t pinR = 8;
+String ssid;     
+String password;
+char buff[64];
+int intssid; 
 
 void ntp_status(){
 int stat = ntp.status();
@@ -79,23 +87,7 @@ void wifiScan() {
         }
     }
 }
-
-void ReadSerial_2(){
-    if(Serial2.available()){
-        char inc = Serial2.read();
-        incStr+=inc;
-        if(inc==0x0A){//конец команды
-            //AnaliseString();
-            incStr = "";
-        }
-    }
-}
-void AnaliseString(void){
-
-    
-}
-//#include "hmi_mini.cpp"
-void UART_RX_IRQ(){Serial.print("irq");}
+//void UART_RX_IRQ(){Serial.print("irq");}
 
 void serialEventRun () {
   if (Serial2.available ()) {
@@ -110,12 +102,7 @@ void serialEventRun () {
     }
   }
 }  
-String ssid;     
-String password;
 
-
-char buff[64];
-int intssid; 
 void read_buf_serial_hmi(){
     //for(int i = 0;i<bytesArraySerial2;i++){Serial.write(sBufSerial2[i]);}
     if(boolean_password){for(int i =1,j=0;i<bytesArraySerial_hmi-3;i++,j++){buff[j]= sBufSerial_hmi[i];}password = String(buff);boolean_password = false;password_ok=true;}
@@ -126,6 +113,7 @@ void read_buf_serial_hmi(){
     else if((sBufSerial_hmi[1]==0x02)&&(sBufSerial_hmi[2]==0x01)&&(sBufSerial_hmi[3]==0x00)){boolean_xz = false;}//exit
     else if((sBufSerial_hmi[1]==0x04)&&(sBufSerial_hmi[2]==0x06)&&(sBufSerial_hmi[3]==0x01)){boolean_password = true;}//exit
     else if((sBufSerial_hmi[1]==0x04)&&(sBufSerial_hmi[2]==0x06)&&(sBufSerial_hmi[3]==0x00)){boolean_ssid = true;}//
+    else if((sBufSerial_hmi[1]==0x01)&&(sBufSerial_hmi[2]==0x16)&&(sBufSerial_hmi[3]==0x01)){upload_clock_hmi();}//
     else if((sBufSerial_hmi[1]==0x05)&&(sBufSerial_hmi[2]==0x0C)&&(sBufSerial_hmi[3]==0x00)){WiFi.disconnect();sendComand(Serial2,"page page0");sendString(Serial2,"wifiConnect.txt", "wifi not connected");sendInt(Serial2,"va10.val",0);sendInt(Serial2,"tm2.en",1);}//exit
     else if((sBufSerial_hmi[1]==0x05)&&(sBufSerial_hmi[2]==0x00)&&(sBufSerial_hmi[3]==0x01)){
        //char localip = WiFi.localIP();
@@ -152,6 +140,45 @@ void read_buf_serial_hmi(){
     for(int i = 0;i<64;i++)buff[i]=0x00;
     }
 
+void vTaskNTPsunc( void * pvParameters )
+{
+    while(true){
+        if(WiFi.status() == WL_CONNECTED) { 
+            ntp.updateNow();   
+            //Serial.println();
+            ntp_status();
+            //Serial.print("синхронизация времени ");Serial.print(" ");Serial.println(String(ntp.ms())+"ms");
+            Serial.println(ntp.timeString());
+            Serial.println(ntp.dateString());
+            Serial.println();
+        }
+        vTaskDelay(DELAY_1minutes);
+    }
+}
+
+void upload_clock_hmi(){
+ ntp.updateNow(); 
+ ntp_status();
+ String time = ntp.timeString();
+ String date = ntp.dateString();
+ String hour = time.substring(0,2) ;
+ String minutes = time.substring(3,5) ;
+ String seconds = time.substring(6,8) ;
+sendInt(Serial_hmi,"rtc3",hour.toInt());
+sendInt(Serial_hmi,"rtc4",minutes.toInt());
+sendInt(Serial_hmi,"rtc5",seconds.toInt());
+//hour.toInt();
+//minutes.toInt();
+//seconds.toInt();
+ Serial.println(time);
+ //Serial.println(date);
+ //Serial.println(ntp.timeString());
+ //Serial.println(ntp.dateString());
+ //Serial.println();
+
+
+}
+
 void setup() {
     // Set WiFi to station mode and disconnect from an AP if it was previously connected
     WiFi.mode(WIFI_STA);
@@ -159,20 +186,15 @@ void setup() {
     Serial.begin(115200);
     Serial2.begin(115200);
     //ntp.begin();
-    //ntp.setPeriod(5);
-    sntp_sync_status_t sync_status;
-    sntp_set_sync_status( sync_status);
-    sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG("pool.ntp.org");
-    esp_netif_sntp_init(&config);
-    if (esp_netif_sntp_sync_wait(pdMS_TO_TICKS(10000)) != ESP_OK) {
-    printf("Failed to update system time within 10s timeout");
-}
-    delay(20);
+    //ntp.setPeriod(60);
+    //static uint8_t ucParameterToPass;
+    //TaskHandle_t xHandle = NULL;
+    //xTaskCreatePinnedToCore( vTaskNTPsunc, "NTPSunhronize", STACK_SIZE, &ucParameterToPass, tskIDLE_PRIORITY, &xHandle, 0 );
+    //xTaskCreatePinnedToCore( vTaskNTPsunc, "NTPSunhronize", STACK_SIZE, &ucParameterToPass, 5, &xHandle, 0 );
+    //configASSERT( xHandle );
     
     sendInt(Serial2,"va10.val",0);
     sendString(Serial2,"wifiConnect.txt", "wifi not connected");sendInt(Serial2,"va10.val",0);sendInt(Serial2,"tm2.en",1);
-    
-    
 }
 
 void loop() {
@@ -201,18 +223,5 @@ void loop() {
         sendInt(Serial2,"va10.val",1);
         //Serial.write((const char*) ssid.c_str());
     }
-    
-if (ntp.ms() == 500) {
-    ntp_status();
-    //if(WiFi.status() == WL_CONNECTED) {
-    delay(2);
-    //digitalWrite(LED_BUILTIN, 0);
-    Serial.print("синхронизация времени ");Serial.print(ntp.ms());
-    Serial.println(ntp.timeString());
-    Serial.println(ntp.dateString());
-    Serial.println();
-  }
-
-    delay(20);
+    delay(10);
 }
-
